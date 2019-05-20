890b9a2aeccc0b1d487bd20376276a8827a71983
rodri369 <46567032+rodri369@users.noreply.github.com>
2019-05-20 11:35:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies,
     Composites = Matter.Composites;
+    Constraint = Matter.Constraint;
 let engine = Engine.create();
 let render = Render.create({
     element: document.body,
@@ -11,13 +12,22 @@ let render = Render.create({
 });
 Engine.run(engine);
 Render.run(render);
- 
+
 let ball=Bodies.circle(200, 10, 40);
-let floor=Bodies.trapezoid(340,300,500,100,.9,{isStatic: true});
-let myCar=Composites.car(390, 0, 100, 30, 40);
-let myCradle=Composites.newtonsCradle(600, 100, 5, 10, 160);
-World.add(engine.world, [ball,floor,myCar,myCradle]);
- 
+let myBall = Bodies.circle(300,40, 60);
+let floor=Bodies.trapezoid(340,500,500,100,.9,{isStatic: false});
+let myCradle=Composites.newtonsCradle(600, 200, 1000, -20, 30);
+let cradle2= Composites.newtonsCradle(700, 300, 400, -5, 30);
+let constraint = Constraint.create({
+    length:250,
+    stiffness:.1,
+    pointA: {x: 250, y:20},
+    bodyB: myBall,
+});
+
+World.add(engine.world, [ball,floor,myCradle,cradle2,myBall,constraint]);
+
+
 let world = engine.world;
 let Mouse= Matter.Mouse;
 let MouseConstraint=Matter.MouseConstraint;
